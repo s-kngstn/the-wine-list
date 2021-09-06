2d6586ab09ccc2064ddbb2d89b6842f881a6bc2f
sk <sk@m1.local>
2021-09-06 11:18:15 +0100

    prepping to stage

--- server.js
+++ server.js
@@ -13,10 +13,20 @@ app.use(cors({
   origin: 'http://localhost:3000',
 }))
 
-app.get("/", (req, res) => res.send("API Running"));
+// app.get("/", (req, res) => res.send("API Running"));
 
 // Define Routes
 app.use("/wine", require("./routes/api/wine"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get("*", (req, res) => {
+    res.sendFire(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 1337;
 
